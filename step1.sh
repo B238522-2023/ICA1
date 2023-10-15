1dd654337d37734690837f7d2980a53b782d27e9
B23852-2023 <s2552113@ed.ac.uk>
2023-10-15 17:06:59 +0100

    modify1 of step1.sh

--- step1.sh
+++ step1.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/bash
-#incase there is already a same ICA1 directory and avoid to execute it for many times
+#remove the fastq incase it is in the directory
 rm -rf fastq
+#copy the data to my ICA1
 cp -r /localdisk/data/BPSM/ICA1/fastq .
 
+#remove fastqc_result to make sure there has no such file before
 rm -rf fastqc_result
 mkdir -p fastqc_result
 
+#use fastq to uncompress the *fq.gz file and output to fastqc_result
+#use for loop to escape the Tco2.ffiles which is not a compressed file 
 for file in fastq/*fq.gz;do
 	fastqc $file -o fastqc_result
 done
