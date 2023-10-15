ee8e7bd5f5308e26a8a7eedcdb2eae74e9ab4068
B23852-2023 <s2552113@ed.ac.uk>
2023-10-16 00:41:21 +0100

    modify1 of step2

--- step2.sh
+++ step2.sh
@@ -2,11 +2,22 @@
 #count the numbers of theoutputs of fastqc files
 echo "the number of the raw sewuence data is：$(ls fastq/*fq.gz | wc -l)"
 
-#remove any existing assesment files
-rm -rf assesment 
-mkdir -p assesment
+#remove any existing fastqc_unzipped and make a fastqc_unziiped directory
+rm -rf fastqc_unzipped
+mkdir -p fastqc_unzipped
+
 
 #decompress the zipped file of the fastqc_result
 for zip_file in fastqc_result/*zip;do
-	unzip $zip_file -d assesment
+	unzip $zip_file -d fastqc_unzipped
+done
+
+#remove any existing directory and make a new one
+rm -rf all_the_summary
+mkdir -p all_the_summary
+
+#change the name of summary.txt and output it to all_the_summary directory
+for folder in fastqc_unzipped/*fastqc;do
+foldername=${$folder/_fastqc/} #definate a foldername delete the filename (_fastqc) part
+mv $folder/summary.txt all_the_summary/"$foldername"_summary.txt #add the name of every summary.txt with its sequence name and output them to a directory called all_the_summary
 done
