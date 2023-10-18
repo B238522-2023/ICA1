176b151aae40b2211994db1950adada39d5d2827
B23852-2023 <s2552113@ed.ac.uk>
2023-10-18 15:28:22 +0100

    succesfully build the index of step3

--- step3.sh
+++ step3.sh
@@ -1,19 +1,68 @@
+#!/usr/bin/bash
 #delete existing Tcongo_genome
 rm -rf Tcongo_genome
 #copy the data of Trypanosoma congolense IL3000 genome to my ICA1
 cp -r /localdisk/data/BPSM/ICA1/Tcongo_genome .
 
-#decompress the file into its present directory
-gunzip Tcongo_genome/TriTrypDB-46_TcongolenseIL3000_2019_Genome.fasta.gz -dfq
+
+#buid a bowtie2 index
+rm -rf *index*
+bowtie2-build Tcongo_genome Tco_example_index
+
 
 rm -rf Tco_1_fastq
 mkdir -p Tco_1_fastq
+cp fastq/*_1.fq.gz Tco_1_fastq
 
 rm -rf Tco_2_fastq
 mkdir -p Tco_2_fastq
+cp fastq/*_2.fq.gz Tco_2_fastq
 
-mv fastq/*_1.fq.gz Tco_1_fastq/
-mv fastq/*_2.fq.gz Tco_2_fastq/
-#buid a bowtie2 index file
-rm -rf *.bt2
-bowtie2-build Tcongo_genome/TriTrypDB-46_TcongolenseIL3000_2019_Genome.fasta Tco_example_index
+
+
+
+
+
+
+
+
+
+for example_1_fastq in Tco_1_fastq/*_1.fq.gz;do
+example_2_fastq="Tco_2_fastq/*_2.fq.gz"
+bowtie2 --end-to-end -p 6 -x Tco_example_index -1 $example_1_fastq -2 $example_2_fastq -S Tco_alignment.sam
+done
+
+rm -rf Tco_alignment.sam
+mkdir Tco_alignment.sam
+
+for example_1_fastq in Tco_1_fastq/*_1.fq.gz;do
+example_2_fastq=${example_1_fastq/_1.fq.gz/_2.fq.gz}
+bowtie2 --end-to-end -p 6 -x Tco_example_index -1 $example_1_fastq -2 $example_2_fastq -S Tco_alignment.sam
+done
+
+
+for file in Tco_1_fastq；do
+bowtie
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#change the format into "BAM"
+samtools sort example.sam > example.bam
+
+
+
+
+
+
+
