25345fa153d0801e41d3fd7d9e54d93f2b3c80f0
B23852-2023 <s2552113@ed.ac.uk>
2023-10-18 01:13:37 +0100

    finish the output step

--- step2.sh
+++ step2.sh
@@ -1,24 +1,25 @@
-#!/usr/bin/bash
-#count the numbers of theoutputs of fastqc files
-echo "the number of the raw sewuence data is：$(ls fastq/*fq.gz | wc -l)"
+#delete existing Tcongo_genome
+rm -rf Tcongo_genome
+#copy the data of Trypanosoma congolense IL3000 genome to my ICA1
+cp -r /localdisk/data/BPSM/ICA1/Tcongo_genome .
 
-#remove any existing fastqc_unzipped and make a fastqc_unziiped directory
-rm -rf fastqc_unzipped
-mkdir -p fastqc_unzipped
+#decompress the file into its present directory
+gunzip Tcongo_genome/TriTrypDB-46_TcongolenseIL3000_2019_Genome.fasta.gz -dfq
+
+rm -rf Tco_1_fastqc
+mkdir -p Tco_1_fastqc
+
+rm -rf Tco_2_fastqc
+mkdir -p Tco_2_fastqc
+
+mv fastqc_unzipped/*_1_fastqc Tco_1_fastqc/
+mv fastqc_unzipped/*_2_fastqc Tco_2_fastqc/
+#buid a bowtie2 index file
+rm -rf *.bt2
+bowtie2-build Tcongo_genome/TriTrypDB-46_TcongolenseIL3000_2019_Genome.fasta TriTrypDB-46_TcongolenseIL3000_2019_Genome
 
 
-#decompress the zipped file of the fastqc_result
-for zip_file in fastqc_result/*zip;do
-	unzip $zip_file -d fastqc_unzipped
-done
-
-#remove any existing directory and make a new one
-rm -rf all_the_summary
-mkdir -p all_the_summary
-
-#rename the summary.txt using the sequence name and output it to all_the_summary directory
-for folder in fastqc_unzipped/*fastqc;do
-foldername=$(basename $folder _fastqc) #definate a foldername delete the filename (_fastqc) part
-mv $folder/summary.txt all_the_summary/"$foldername"_summary.txt #add the name of every summary.txt with its sequence name and output them to a directory called all_the_summary
-done
-
+#using bowtie2 to get "SAM" format output
+bowtie2 -p 6 -3 5 --local -x TriTrypDB-46_TcongolenseIL3000_2019_Genome -1 Tco_1_fastqc/*_1_fastqc -2 Tco_2_fastqc/*_2_fastqc -S example.sam
+#change the format into "BAM"
+samtools sort example.sam > example.bam
