2f4b423a3c164761dd7d8ffd8edd8fc560b2081c
B23852-2023 <s2552113@ed.ac.uk>
2023-10-19 09:54:43 +0100

    modify of bowtie2 code of step3

--- step3.sh
+++ step3.sh
@@ -6,18 +6,17 @@ cp -r /localdisk/data/BPSM/ICA1/Tcongo_genome .
 
 
 #buid a bowtie2 index
-rm -rf *index*
+rm -rf Tcongo_genome/*index*
 bowtie2-build Tcongo_genome/*fasta.gz Tcongo_genome/Tcongo_genome_index
 
 
 rm -rf Tco_alignment_sam
 mkdir Tco_alignment_sam
 
-for fqfile in fastq/*_1.fq.gz;do 		#fqfile = fastq/Tco-999_1.fq.gz
-	name1=$(basename $fqfile) 			#Tco-999_1.fq.gz
-	name2=${name1/_1.fq.gz/_2.fq.gz}	#Tco-999_2.fq.gz
-	outname=${name1/_1.fq.gz/.sam}		#Tco-999.sam
+for fqfile in fastq/*_1.fq.gz;do 		
+	name1=$(basename $fqfile) 	
+	name2=${name1/_1.fq.gz/_2.fq.gz}
+	outname=${name1/_1.fq.gz/.sam}		
 
-	bowtie2 -x Tcongo_genome/Tcongo_genome_index -1 fastq/"$name1"  -2 fastq/"$name2"  -S Tco_alignment_sam/"$outname"
-	#echo "1" #debugging
+	bowtie2 -p 6 -x Tcongo_genome/Tcongo_genome_index -1 fastq/"$name1"  -2 fastq/"$name2"  -S Tco_alignment_sam/"$outname"
 done
