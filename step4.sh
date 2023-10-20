2091413505cfe06a42e4a37df2991f31df2a2f08
B23852-2023 <s2552113@ed.ac.uk>
2023-10-20 21:59:59 +0100

    output the outcome to txt file in step4

--- step4.sh
+++ step4.sh
@@ -3,10 +3,14 @@
 rm -rf TriTrypDB-46_TcongolenseIL3000_2019.bed
 cp -r /localdisk/data/BPSM/ICA1/TriTrypDB-46_TcongolenseIL3000_2019.bed .
 
-#generate counts data
-for bamfile in Tco_sort/*.bam;do
-	inputfile=${bamfile}
-	bedtools multicov -bams "$inputfile" -bed TriTrypDB-46_TcongolenseIL3000_2019.bed
- 
 
+
+#generate the counts data
+rm -rf Tco_sort/*.txt
+for bamfile in Tco_sort/*.bam;do
+output=${bamfile/.bam/*.txt}
+bedtools multicov -bams $bamfile -bed TriTrypDB-46_TcongolenseIL3000_2019.bed >> $output 
 done 
+
+
+for file in Tco_sort/*txt;do
