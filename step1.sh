6ea13d4ef86ae5e1d3f0fbfe1a426df904df76d2
B23852-2023 <s2552113@ed.ac.uk>
2023-10-21 18:46:25 +0100

    change the spell mistake in step1

--- step1.sh
+++ step1.sh
@@ -9,7 +9,7 @@ rm -rf fastqc_result
 #make a fastqc_result directory
 mkdir -p fastqc_result
 
-#use fastq to decompress the *fq.gz file and output to fastqc_result
+#use fastqc to decompress the *fq.gz file and output to fastqc_result
 #use for loop to escape the Tco2.ffiles which is not a compressed file 
 for file in fastq/*fq.gz;do
 	fastqc $file -o fastqc_result
