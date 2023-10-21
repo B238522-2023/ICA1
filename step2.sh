9c307197cc8ccedd66ed6870590ece0b75c9d2c8
B23852-2023 <s2552113@ed.ac.uk>
2023-10-21 21:28:14 +0100

    modify the code of the quality assesment of the raw sequence

--- step2.sh
+++ step2.sh
@@ -1,16 +1,8 @@
-#!/usr/bin/bash
-#count the numbers of theoutputs of fastqc files
-echo "the number of the raw sewuence data is：$(ls fastq/*fq.gz | wc -l)"
-
-#remove any existing fastqc_unzipped and make a fastqc_unziiped directory
-rm -rf fastqc_unzipped
-mkdir -p fastqc_unzipped
-
-
-#decompress the zipped file of the fastqc_result
+#extract the zipped file of the fastqc_result
 for zip_file in fastqc_result/*zip;do
-	unzip $zip_file -d fastqc_unzipped
+	unzip $zip_file -d fastqc_unzip
 done
+
 
 #remove any existing directory and make a new one
 rm -rf all_the_summary
@@ -18,18 +10,18 @@ mkdir -p all_the_summary
 
 #rename the summary.txt using the sequence name and output it to all_the_summary directory
 for folder in fastqc_unzipped/*fastqc;do
-foldername=$(basename $folder _fastqc) #definate a foldername delete the filename (_fastqc) part
+	foldername=$(basename $folder _fastqc) #definate a foldername delete the filename (_fastqc) part
 mv $folder/summary.txt all_the_summary/"$foldername"_summary.txt #add the name of every summary.txt with its sequence name and output them to a directory called all_the_summary
 done
-#drag all the summary into a new file for quality assesment
+
 
 #asses the quality of summary.txt,echo the number of 'PASS' of each file
 count=0
 for file in all_the_summary/*.txt
 do
 	count=$(grep -c "PASS" $file)
-	if test $count -gt 5
+if test $count -gt 7
 	then
-		echo -e "The number of PASS in $file is $count"
+	echo -e "The number of PASS in $file is $count,and the quality of raw sequence data is good"
 	fi
 done 
