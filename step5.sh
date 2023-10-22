6980769c7098b820e71214c063067449a2730c13
B23852-2023 <s2552113@ed.ac.uk>
2023-10-22 23:31:36 +0100

    generate the average of the counts in step5

--- step5.sh
+++ step5.sh
@@ -1,9 +1,35 @@
 #!/usr/bin/bash
-rm -rf Tco_sort/*_average.txt
-#cut the genename,gene expression and count field to a new file
-for file in Tco_sort/*.txt;do
-	output=${file/.txt/_average.txt}
-	input=$(cut -f 4,5,6 $file | uniq -c |sort -k1,1nr)
-	average=$(cut -f6 $file | awk '{sum+=$1} END {print "Average=" sum/NR}')
-	echo -e "###The average of the count is:$average\t$input" >> $output
+rm -rf group
+mkdir -p group
+
+#interate from file group/g1.txt to group/g15.txt 
+for i in {1..15};do
+        touch group/g"$i"_all.txt
+        outfile=group/g"$i"_all.txt
+#paste colum6 of the file in same group into a midfile for further execution
+        for gi in groupfile/*g"$i".txt ;do
+#
+                midfile=group/g"$i"_mid.txt
+
+                paste $outfile <(cut -f 6 "$gi") > $midfile
+                mv $midfile $outfile
+        done
+done
+
+for all_file in group/* ;do
+        num_col=$(awk -F '\t' '{print NF}' $all_file | head -1 )
+        echo $num_col
+        awk -F "\t" -v nc=$num_col '{
+        sum=0;
+        for( i = 2 ; i <= nc l; i++) {
+          sum += $i;
+        }
+        avg=sum/nc;
+        { print avg }
+        }' $all_file > "$all_file"_mid
+done
+
+for midfile in group/*mid ; do
+        outputname=$(basename $midfile .txt_mid)"_avg.txt"
+        paste <(cut TriTrypDB-46_TcongolenseIL3000_2019.bed -f 4,5) $midfile > gr>
 done
