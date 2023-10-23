57a0b30e1e1824ea5485abbbe98b31ba88a8d965
B23852-2023 <s2552113@ed.ac.uk>
2023-10-23 01:50:36 +0100

    add 2 more foldchange

--- step6.sh
+++ step6.sh
@@ -3,7 +3,7 @@ rm -rf foldchange
 mkdir -p foldchange
 
 
-#generate the foldchange of clone1_T0_Uninduced_g1 and WT_T0_Uninduced_g11
+#1)different type,same time,same treatment: clone1_T0_Uninduced_g1 and WT_T0_Uninduced_g11
 #if the count in WT group is 0,then change it into one to calculate
 paste "group/g1_all_avg.txt" "group/g11_all_avg.txt" | awk -F "\t" '
 BEGIN{ OFS = "\t" }
@@ -12,3 +12,19 @@ if ($6 == 0){foldchange=($3/1);}
 else{foldchange=($3/$6);}
         {print $1,$2,foldchange;}
 }' > foldchange/g1_g11.txt
+#2)same type,different time,same treatment:CL1_T24_Uninduced_g3 and CL1_T48_Uninduced_g5
+paste "group/g3_all_avg.txt" "group/g5_all_avg.txt" | awk -F "\t" '
+BEGIN{ OFS = "\t" }
+{
+if ($6 == 0){foldchange=($3/1);}
+else{foldchange=($3/$6);}
+        {print $1,$2,foldchange;}
+}' > foldchange/g3_g5.txt
+#3)same type,same time,different treatment:WT_T48_Induced_g14 and WT_T48_Uninduced_g15
+paste "group/g14_all_avg.txt" "group/g15_all_avg.txt" | awk -F "\t" '
+BEGIN{ OFS = "\t" }
+{
+if ($6 == 0){foldchange=($3/1);}
+else{foldchange=($3/$6);}
+        {print $1,$2,foldchange;}
+}' > foldchange/g14_g15.txt
